d7bc318c7950a211ad606f10b172b06794f94801
Calvin Putera Loka <calvinputeraloka@gmail.com>
2021-12-23 23:17:15 +0700

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import logo from './assets/logo.svg';
+import mockups from './assets/illustration-mockups.svg';
+import { TiSocialInstagramCircular, TiSocialFacebookCircular, TiSocialTwitterCircular } from "react-icons/ti";
 import './App.css';
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='bg-image'></div>
+      <div class="bg-image-mobile"></div>
+      <img src={ logo } alt="logo" className='logo' />
+      <div class="content">
+        <img src={ mockups } alt="mockups" />
+        <div class="content-text">
+          <h1>Build The Community Your Fans Will Love</h1>
+          <p>Huddle re-imagines the way we build communities. You have a voice, but so does your audience.
+            Create connections with your users as you engage in genuine discussion. </p>
+          <button className='btn'>Register</button>
+        </div>
+        <div class="social">
+          <a href="/">
+            <TiSocialFacebookCircular style={ { width: "50px", height: "50px" } } />
+          </a>
+          <a href="/">
+            <TiSocialTwitterCircular style={ { width: "50px", height: "50px" } } />
+          </a>
+          <a href="/">
+            <TiSocialInstagramCircular style={ { width: "50px", height: "50px" } } />
+          </a>
+        </div>
+      </div>
+    </>
   );
 }
 
